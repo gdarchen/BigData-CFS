c942bb72aa621d97fce2b62b89ac2b7bc3f08335
Gautier DARCHEN <darchen.gautier@gmail.com>
2017-12-13 13:27:29 +0100

    Modifs mineures server

--- server/server.py
+++ server/server.py
@@ -3,16 +3,17 @@ from cassandra.cluster import Cluster
 class Server(object):
 
     def __init__(self):
-        cluster = Cluster()
-        session = cluster.connect('tmp')
+        self.cluster = Cluster()
+        self.session = self.cluster.connect('tmp')
     
     def print_table_rows(self):
-        rows = session.execute('SELECT * FROM tmp_table')
+        rows = self.session.execute('SELECT * FROM tmp_table')
+        print(rows)
         for row in rows:
             print(row)
 
     def print_table_row_with_id(self, row_id):
-        rows = session.execute('SELECT * FROM tmp_table where id=%d' % row_id)
+        rows = self.session.execute('SELECT * FROM tmp_table where id=%d' % row_id)
         for row in rows:
             print(row)
 
