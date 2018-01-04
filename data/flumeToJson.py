14caf3feef6af8685d3a3c2c2716e69732328660
Alexandre Huat <alexandre.huat@gmail.com>
2018-01-04 12:59:58 +0100

    modif flumetojson + add vérité terrain json

--- data/flumeToJson.py
+++ data/flumeToJson.py
@@ -8,23 +8,27 @@ if __name__ == "__main__":
     parser.add_argument('--flume-file', help='path of the FlumeData File')
     args = parser.parse_args()
 
-    print("\n##################START\n")
-    print("typing 'break' will exit the script and save the labeled texts\n")
+    print("START")
+    print("Labelling will stop when you've juged 200 tweets.")
+    print("Labelling must answer the question \"Is the tweet positive, neutral or negative about Iron Man 3?\"")
+
     file_to_convert = args.flume_file
     new_json = OrderedDict()
     # FlumeData to Convert
     data = open(file_to_convert)
 
+    no = 0
     for tweet in data:
         try:
             tweet_json = json.loads(tweet)
             # ignore the tweets that are not French or English
             if tweet_json['lang'] not in ['en','fr']:
                 continue
-            print(tweet_json['text'])
+            no += 1
+            print("{}.".format(no), tweet_json['text'])
             # here you write the label for the text
-            label = input("label (1, 0, -1)?  ")
-            if label == "break":
+            label = input("Label (1, 0 or -1):\t")
+            if no == 200:
                 break
             new_json[tweet_json['text']] = label
         except KeyError:
@@ -32,4 +36,4 @@ if __name__ == "__main__":
     with open(args.flume_file+'.json', 'w') as outfile:
         json.dump(new_json, outfile, ensure_ascii=True)
 
-    print("\n##################FINISH\n")
+    print("\nFINISHED")
